e0eab78c061518f86a75ed1ca2dc4e2145012f0e
Protryon <max.bruce12@gmail.com>
2023-12-27 13:04:10 -0800

    add leakagent demo

---
+++ leakagent/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+here=$(realpath $(dirname "$0"))
+cd "$here"
+
+helm upgrade --install leakagent oci://registry-1.docker.io/leaksignal/leakagent \
+  --version 0.8.0-helm \
+  -f ./leakagent.yaml \
+  --namespace leakagent --create-namespace
+
+helm upgrade --install leaksignal-operator oci://registry-1.docker.io/leaksignal/leaksignal-operator \
+  --version 1.1.0-helm \
+  --namespace kube-system
+
+kubectl apply -f ./ns.yaml
+
+kubectl apply -n leakagent-demo -f ./boutique
+
+kubectl apply -n leakagent-demo -f ./leaksignal.yaml
